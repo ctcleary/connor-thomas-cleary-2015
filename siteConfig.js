254b91976d64811898f8f4651c5c164f8e297f3b
Connor T Cleary <connorcleary@gmail.com>
2015-08-04 08:27:08 -0700

    Nesting nav items.

--- siteConfig.js
+++ siteConfig.js
@@ -1,29 +1,29 @@
 var siteConfigJson = {
   nav: {
-    other: 'other',
-    items: [
+    label: 'Site Navigation',
+    navItems: [
       {
-        navLabel: 'Writer',
+        label: 'Writer',
         subNavItems: [
-          { navLabel: 'Interactive' },
-          { navLabel: 'Fictional' },
-          { navLabel: 'Journalism' }
+          { label: 'Interactive', subNavItems: [] },
+          { label: 'Fictional', subNavItems: [] },
+          { label: 'Journalism', subNavItems: [] }
         ]
       },
       {
-        navLabel: 'Developer',
+        label: 'Developer',
         subNavItems: [
-          { navLabel: 'Front-End' },
-          { navLabel: 'Scripting' },
-          { navLabel: 'Object Oriented' }
+          { label: 'Front-End', subNavItems: [] },
+          { label: 'Scripting', subNavItems: [] },
+          { label: 'OOP', subNavItems: [] }
         ]
       },
       {
-        navLabel: 'Designer',
+        label: 'Designer',
         subNavItems: [
-          { navLabel: 'Game' },
-          { navLabel: 'Visual' },
-          { navLabel: 'Motion' }
+          { label: 'Game', subNavItems: [] },
+          { label: 'Visual', subNavItems: [] },
+          { label: 'Motion', subNavItems: [] }
         ]
       }
     ]
@@ -32,7 +32,7 @@ var siteConfigJson = {
     items: [
       {
         itemRel: 'writer:interactive',
-        itemTitle: 'stunningContent',
+        label: 'stunningContent',
         paragraphs: [
           'Maecenas eu placerat ante. Fusce ut neque justo, et aliquet enim. In hac habitasse platea dictumst. Nullam commodo neque erat, vitae facilisis erat. Cras at mauris ut tortor vestibulum fringilla vel sed metus. Donec interdum purus a justo feugiat rutrum. Sed ac neque ut neque dictum accumsan. Cras lacinia rutrum risus, id viverra metus dictum sit amet. Fusce venenatis, urna eget cursus placerat, dui nisl fringilla purus, nec tincidunt sapien justo ut nisl. Curabitur lobortis semper neque et varius. Etiam eget lectus risus, a varius orci. Nam placerat mauris at dolor imperdiet at aliquet lectus ultricies. Duis tincidunt mi at quam condimentum lobortis.',
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac magna non augue porttitor scelerisque ac id diam. Mauris elit velit, lobortis sed interdum at, vestibulum vitae libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque iaculis ligula ut ipsum mattis viverra. Nulla a libero metus. Integer gravida tempor metus eget condimentum. Integer eget iaculis tortor. Nunc sed ligula sed augue rutrum ultrices eget nec odio. Morbi rhoncus, sem laoreet tempus pulvinar, leo diam varius nisi, sed accumsan ligula urna sed felis. Mauris molestie augue sed nunc adipiscing et pharetra ligula suscipit. In euismod lectus ac sapien fringilla ut eleifend lacus venenatis.',
@@ -41,7 +41,7 @@ var siteConfigJson = {
       },
       {
         itemRel: 'writer:fictional',
-        itemTitle: 'amazingContent',
+        label: 'amazingContent',
         paragraphs: [
           'Integer elementum massa at nulla placerat varius. Suspendisse in libero risus, in interdum massa. Vestibulum ac leo vitae metus faucibus gravida ac in neque. Nullam est eros, suscipit sed dictum quis, accumsan a ligula. In sit amet justo lectus. Etiam feugiat dolor ac elit suscipit in elementum orci fringilla. Aliquam in felis eros. Praesent hendrerit lectus sit amet turpis tempus hendrerit. Donec laoreet volutpat molestie. Praesent tempus dictum nibh ac ullamcorper. Sed eu consequat nisi. Quisque ligula metus, tristique eget euismod at, ullamcorper et nibh. Duis ultricies quam egestas nibh mollis in ultrices turpis pharetra. Vivamus et volutpat mi. Donec nec est eget dolor laoreet iaculis a sit amet diam.',
           'Nullam eros mi, mollis in sollicitudin non, tincidunt sed enim. Sed et felis metus, rhoncus ornare nibh. Ut at magna leo. Suspendisse egestas est ac dolor imperdiet pretium. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porttitor, erat sit amet venenatis luctus, augue libero ultrices quam, ut congue nisi risus eu purus. Cras semper consectetur elementum. Nulla vel aliquet libero. Vestibulum eget felis nec purus commodo convallis. Aliquam erat volutpat.',
