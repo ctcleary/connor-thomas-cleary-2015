90460b2f84c0fb5af329196b645c62a6eb16d12b
Connor T Cleary <connorcleary@gmail.com>
2015-08-06 23:30:07 -0700

    WIP Update navigation transitions.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,22 +11,54 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      files: '**/*.*',
-      tasks: [],
-      options: {
-        livereload: true
+      all: {
+        files: '**/*.*',
+        tasks: [],
+        options: {
+          livereload: true
+        }
+      },
+      sass: {
+        files: 'style/scss/*.scss',
+        tasks: ['buildcss']
+      }
+    },
+
+    sass: {
+      dev: {
+        files: {
+          'style/styles.css': 'style/scss/master.scss'
+        }
+      }
+    },
+    autoprefixer: {
+      dev: {
+        options: {
+          browsers: ['last 4 versions']
+        },
+        files: {
+          'style/styles.css': 'style/styles.css'
+        }
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', [
+    'buildcss',
     'connect',
     'watch'
+  ]);
+
+  grunt.registerTask('buildcss', [
+    'sass',
+    'autoprefixer'
   ]);
 
 };
