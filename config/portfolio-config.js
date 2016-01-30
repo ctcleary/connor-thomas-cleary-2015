4bf173f412b989c301d2f0327b59d6f1e3ea284f
Connor T Cleary <connorcleary@gmail.com>
2016-01-30 13:17:48 -0800

    Style tweaks.

--- config/portfolio-config.js
+++ config/portfolio-config.js
@@ -33,7 +33,7 @@ var portfolioItems = [
       },
       description: ['.item-desc',
         ['p', 'As a hacker-for-hire, you\'ve stumbled into a system unlike any you\'ve ever encountered. Now you\'re a prisoner and a lab rat, but you have someone on the outside.'],
-        ['p', 'What kind of a test subject will you be?'],
+        ['p', 'What kind of test subject will you be?'],
         ['p', 'This interactive fiction features branching storylines, stat tracking, and five-ish endings to unlock.'],
         ['p', ['em', 'Finalist entry in the one-month game writing jam, ', ['a', {href:'http://itch.io/jam/wag-challenge', target:'_blank'}, '2015 Write-A-Game Challenge'], ' (#WagChallenge), supported by the IGDA Game Writing SIG. The theme was “Down the Rabbit Hole.”']]
       ],
